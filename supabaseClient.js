446f27dd7723f8d6cabe93b04fe2787af5729ee2
William Gabriel Silva <127068960+WilliamGabrielSilva@users.noreply.github.com>
2025-05-20 22:35:11 -0300

    d

--- supabaseClient.js
+++ supabaseClient.js
@@ -5,5 +5,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
   'https://pgqrjtglzibalkjlkrxt.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBncXJqdGdsemliYWxramxrcnh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2OTQ2NjEsImV4cCI6MjA2MzI3MDY2MX0.0ryT_WIlWOBREOetGVCJBu0hCb2TttkDGXpim4o_-C4'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBncXJqdGdsemliYWxramxrcnh0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzY5NDY2MSwiZXhwIjoyMDYzMjcwNjYxfQ.tlYQRgXgxpQ8YrpaP-1_9vOB547K_ZH23HkEr2g92oI'
 )
+
